8fbcc2063c78e188c93afe62d74d563bfb2e5b68
onlyfullstack <onlyfullstackdev@gmail.com>
2019-10-24 17:06:33 +0530

    Added Class Component

--- Notification.js
+++ Notification.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
 export default class Notification extends Component {
-render() {
-  return(
-    <h3>you have {this.props.notification} new notifications</h3>
-  );
-}
+  render() {
+    var message;
+    if (this.props.notification) {
+      message = <h3>you have {this.props.notification} new notifications</h3>;
+    } else {
+      message = <h3>You have {this.props.notification} new notifications</h3>;
+    }
 
+    return message;
+  }
 }
