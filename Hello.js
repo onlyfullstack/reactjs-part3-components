8d40306bd50dafab8c50e50cf4c0580e0f356cd3
onlyfullstack <onlyfullstackdev@gmail.com>
2019-10-25 21:48:12 +0530

    Simple Components

--- Hello.js
+++ Hello.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+export default ({  }) => <h1>Hello Only Fullstack</h1>;
--- Notification.js
+++ Notification.js
@@ -4,27 +4,18 @@ import Hello from "./Hello";
 import "./style.css";
 
 export default class Notification extends Component {
-  constructor() {
-    this.state = {
-      component: "Notification"
-    };
-  }
-
+  
   render() {
-    var message;
-    var displayMessage = (this.props.notification > 0) ? "You have " + this.props.notification + " new notifications" : "You dont have new notifications";
-
-      message = (
+    
+    return (
         <div>
           <h3 className="notification">
-            {displayMessage}
+            You have 5 new notifications
           </h3>
           <h4 className="notification-component">
-            I am coming from {this.state.component} Component
+            I am coming from Notification Component
           </h4>
         </div>
       );
-    
-    return message;
   }
 }
