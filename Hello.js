4a37be5c54d875b13c6f6b06fbec917c47ee38c1
onlyfullstack <onlyfullstackdev@gmail.com>
2019-11-01 13:53:11 +0530

    functional components

--- Hello.js
+++ Hello.js
@@ -1,3 +1,8 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+// export default ({ name }) => <h1>Hello {name}!</h1>;
+
+
+export default function Hello(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
