1ed5edc6d7c174d39708a839af29b59fd621c499
onlyfullstack <onlyfullstackdev@gmail.com>
2019-11-12 10:03:47 +0530

    1 file modified

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ class App extends Component {
         <Hello name={this.state.name} />
         <Notification notification={this.state.notification} />
         <p>Start editing to see some magic happen :)</p>
+        <a
+          href="https://onlyfullstack.blogspot.com/2019/11/react-tutorial.html"
+          target="_blank">
+          ReactJS Tutorial on Only Fullstack
+        </a>
+        <a
+          href="https://onlyfullstack.blogspot.com/2019/11/react-tutorial.html"
+          target="_blank">
+          <img
+            src="https://1.bp.blogspot.com/-I276A9NSgjY/Xck5TadIjpI/AAAAAAAAAi0/gFb-9achXtk4XkELxVyRNR3UXuMJGsUfQCLcBGAsYHQ/s640/React%2BTutorial.jpg" height="30%" width="90%"/>
+        </a>
       </div>
     );
   }
